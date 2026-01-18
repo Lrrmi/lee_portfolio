308a60bf337ac44148161ffe239142015645e0bd
AliA12336 <amadams12336@gmail.com>
2026-01-17 22:56:48 -0500

    Update links to navigate user

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,31 +1,58 @@
 import {
   NavigationMenu,
   NavigationMenuItem,
-  NavigationMenuLink,
   NavigationMenuList,
 } from "@/components/ui/navigation-menu"
 import logo from "../../src/assets/logo/logo192.png";
+import { Link } from "@tanstack/react-router";
 
+// have to use ! b/c the styling from shadcn overrides the tanstack component styles
 export const Navbar = () => {
     return (
     <NavigationMenu>
   <NavigationMenuList className="mt-5 md:gap-7">
     <NavigationMenuItem>
-      <NavigationMenuLink className="hover:bg-transparent hover:text-inherit md:m-2">
-        <img className="md:h-20 w-auto" src={logo} alt="Logo" />
-        </NavigationMenuLink>
+        <Link 
+        to="/" className="hover:bg-transparent hover:text-inherit md:m-2">
+            <img className="md:h-20 w-auto" src={logo} alt="Logo" />
+        </Link>
       </NavigationMenuItem>
     <NavigationMenuItem>
-      <NavigationMenuLink className="text-[#dfdedf] hover:bg-transparent hover:text-none">projects</NavigationMenuLink>
+        <Link
+        to="/projects"
+        className="!text-[#dfdedf] !hover:bg-transparent !hover:text-inherit !md:m-2"
+        activeProps={{
+          className: "text-foreground underline",
+        }}
+      >projects</Link>
     </NavigationMenuItem>
     <NavigationMenuItem>
-      <NavigationMenuLink className="text-[#dfdedf] hover:bg-transparent hover:text-none">about</NavigationMenuLink>
+      <Link 
+      to="/about" 
+      className="!text-[#dfdedf] !hover:bg-transparent !hover:text-inherit !md:m-2"
+      activeProps={{
+          className: "text-foreground underline",
+        }}
+      >about</Link>
     </NavigationMenuItem>
     <NavigationMenuItem>
-      <NavigationMenuLink className="text-[#dfdedf] hover:bg-transparent hover:text-none">contact</NavigationMenuLink>
+      <Link 
+      to="/contact" 
+      className="!text-[#dfdedf] !hover:bg-transparent !hover:text-inherit !md:m-2"
+      activeProps={{
+          className: "text-foreground underline",
+        }}
+      >contact</Link>
     </NavigationMenuItem>
     <NavigationMenuItem>
-      <NavigationMenuLink className="text-[#dfdedf] hover:bg-transparent hover:text-none">potpourri</NavigationMenuLink>
+      <Link 
+      to="/potpourri" 
+      className="!text-[#dfdedf] !hover:bg-transparent !hover:text-inherit !md:m-2"
+      activeProps={{
+          className: "text-foreground underline",
+        }}
+      >potpourri
+      </Link>
     </NavigationMenuItem>
   </NavigationMenuList>
 </NavigationMenu>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,2 +1,3 @@
 export const HomePage = () => {
+    return <div>Home</div>
 }
