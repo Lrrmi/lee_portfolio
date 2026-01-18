e2031fcf8e457980c39a218f298612469acbf7f4
AliA12336 <amadams12336@gmail.com>
2026-01-18 17:21:47 -0500

    Add contact page

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -5,4 +5,52 @@ import { Separator } from "@/components/ui/separator";
 // Separator at bottom
 // nothing on right side
 
-export const ContactPage = () => {};
+export const ContactPage = () => {
+    return (
+        <>
+        <div className="flex flex-row flex-1 text-[#dfdedf]"><div className="contact">
+            <div>
+                <br />
+                <h2 className="text-4xl">Contact</h2>
+                <br />
+                <p>
+                    For inquire about new projects or design services please email me at:
+                </p>
+                <br />
+                <div>
+                    <p>LinkedIn:
+                    <a className="underline ml-1 md:ml-50" href="https://linkedin.com" target="_blank" rel="noopener noreferrer">https://www.linkedin.com/in/robertleeadams</a> 
+                    </p>
+                </div>
+                <br />
+                <div>
+                    <p>Email:
+                    <span className="ml-1 md:ml-55">rmladams25@gmail.com</span>
+                    </p>
+                </div>
+                <br />
+                <br />
+                <h2 className="text-4xl">Services</h2>
+                <br />
+                <div>
+                    <p>Product & Furniture Design Services</p>
+                    <br />
+                    <p>3D Concept/Render Modeling & Production Modeling</p>
+                    <br />
+                    <p>Rendering & Drafting</p>
+                    <br />
+                    <p>Small & Full Scale Prototyping services</p>
+                    <br />
+                    <br />
+                    <br />
+                </div>
+                
+                <br />
+            </div>
+        </div></div>
+
+            <Separator />
+            <br /><br />
+        </>
+    )
+};
