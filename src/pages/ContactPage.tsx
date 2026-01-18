6cbfac2f7eec0625a0292faf98978f555e240a2d
AliA12336 <amadams12336@gmail.com>
2026-01-18 17:25:57 -0500

    Remove comments

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,10 +1,5 @@
 import { Separator } from "@/components/ui/separator";
 
-// flex box
-// Contact text box on left side
-// Separator at bottom
-// nothing on right side
-
 export const ContactPage = () => {
     return (
         <>
