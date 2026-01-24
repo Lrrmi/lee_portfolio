6153ca16e264565d81fab7158f909e242d19d8cb
AliA12336 <amadams12336@gmail.com>
2026-01-24 12:02:55 -0500

    Add top and down arrows

--- src/components/PotpourriGallery.tsx
+++ src/components/PotpourriGallery.tsx
@@ -1,6 +1,26 @@
 import Gallery from "@/components/ui/gallery";
 import { galleryImages } from "@/scripts/convertImages";
+import { Button } from "@/components/ui/button";
+import { MoveUp, MoveDown } from "lucide-react";
 
 export const PotpourriGallery = () => {
-	return <Gallery images={galleryImages} />;
+    const scrollAmount = 1500; 
+
+	return (
+        <div className="relative">
+    <Gallery images={galleryImages} />
+    <Button
+        className="hover:bg-transparent fixed bottom-11 right-12 -translate-x-1/2 bg-transparent p-3 hidden sm:block"
+        onClick={() => window.scrollBy({ top: -scrollAmount, behavior: "smooth" })}
+      >
+        <MoveUp className="!w-6 !h-6" />
+      </Button>
+      <Button
+        className="hover:bg-transparent fixed bottom-4 right-12 -translate-x-1/2 bg-transparent p-3 hidden sm:block"
+        onClick={() => window.scrollBy({ top: scrollAmount, behavior: "smooth" })}
+      >
+        <MoveDown className="!w-6 !h-6"/>
+      </Button>
+    </div>
+);
 };
