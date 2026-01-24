5c004eac102d880d34395dd6bb0396d0d81a3f80
AliA12336 <amadams12336@gmail.com>
2026-01-24 12:03:38 -0500

    Format code

--- src/components/PotpourriGallery.tsx
+++ src/components/PotpourriGallery.tsx
@@ -1,26 +1,30 @@
+import { MoveDown, MoveUp } from "lucide-react";
+import { Button } from "@/components/ui/button";
 import Gallery from "@/components/ui/gallery";
 import { galleryImages } from "@/scripts/convertImages";
-import { Button } from "@/components/ui/button";
-import { MoveUp, MoveDown } from "lucide-react";
 
 export const PotpourriGallery = () => {
-    const scrollAmount = 1500; 
+	const scrollAmount = 1500;
 
 	return (
-        <div className="relative">
-    <Gallery images={galleryImages} />
-    <Button
-        className="hover:bg-transparent fixed bottom-11 right-12 -translate-x-1/2 bg-transparent p-3 hidden sm:block"
-        onClick={() => window.scrollBy({ top: -scrollAmount, behavior: "smooth" })}
-      >
-        <MoveUp className="!w-6 !h-6" />
-      </Button>
-      <Button
-        className="hover:bg-transparent fixed bottom-4 right-12 -translate-x-1/2 bg-transparent p-3 hidden sm:block"
-        onClick={() => window.scrollBy({ top: scrollAmount, behavior: "smooth" })}
-      >
-        <MoveDown className="!w-6 !h-6"/>
-      </Button>
-    </div>
-);
+		<div className="relative">
+			<Gallery images={galleryImages} />
+			<Button
+				className="hover:bg-transparent fixed bottom-11 right-12 -translate-x-1/2 bg-transparent p-3 hidden sm:block"
+				onClick={() =>
+					window.scrollBy({ top: -scrollAmount, behavior: "smooth" })
+				}
+			>
+				<MoveUp className="!w-6 !h-6" />
+			</Button>
+			<Button
+				className="hover:bg-transparent fixed bottom-4 right-12 -translate-x-1/2 bg-transparent p-3 hidden sm:block"
+				onClick={() =>
+					window.scrollBy({ top: scrollAmount, behavior: "smooth" })
+				}
+			>
+				<MoveDown className="!w-6 !h-6" />
+			</Button>
+		</div>
+	);
 };
