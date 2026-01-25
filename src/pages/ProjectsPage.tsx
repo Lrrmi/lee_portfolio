7a86afc31a32743249294cf0d523fd4acf9bdd1b
AliA12336 <amadams12336@gmail.com>
2026-01-25 15:50:24 -0500

    Add arrows to projects page

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -1,10 +1,14 @@
 import { Link } from "@tanstack/react-router";
+import { MoveDown, MoveUp } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
 import { projectRoute } from "@/router";
 
 import projs from "../images.json";
+import { Button } from "@/components/ui/button";
 
 export const ProjectsPage = () => {
+	const scrollAmount = 1500;
+
 	return (
 		<div className="flex">
 			<div className="hidden md:flex md:flex-col w-1/8 gap-2 mt-20 text-[#dfdedf]">
@@ -47,6 +51,22 @@ export const ProjectsPage = () => {
 					</div>
 				</div>
 			</div>
+			<Button
+					className="hover:bg-transparent fixed bottom-11 right-12 -translate-x-1/2 bg-transparent p-3 hidden sm:block"
+					onClick={() =>
+						window.scrollBy({ top: -scrollAmount, behavior: "smooth" })
+					}
+				>
+					<MoveUp className="!w-6 !h-6" />
+				</Button>
+				<Button
+					className="hover:bg-transparent fixed bottom-4 right-12 -translate-x-1/2 bg-transparent p-3 hidden sm:block"
+					onClick={() =>
+						window.scrollBy({ top: scrollAmount, behavior: "smooth" })
+					}
+				>
+					<MoveDown className="!w-6 !h-6" />
+				</Button>
 		</div>
 	);
 };
