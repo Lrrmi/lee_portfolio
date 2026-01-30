d8226856a4c5206cca144d42b56f57557a8623a4
AliA12336 <amadams12336@gmail.com>
2026-01-29 21:45:39 -0500

    Fix the physics following the rigidbody

--- src/components/home/TestScene.tsx
+++ src/components/home/TestScene.tsx
@@ -1,8 +1,9 @@
+import { RigidBodyType } from "@dimforge/rapier3d-compat";
 import { DragControls, useGLTF } from "@react-three/drei";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { Physics, RapierRigidBody, RigidBody } from "@react-three/rapier";
-import { RigidBodyType } from "@dimforge/rapier3d-compat"
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { Physics, type RapierRigidBody, RigidBody } from "@react-three/rapier";
 import { type RefObject, Suspense, useRef } from "react";
+import * as THREE from "three";
 import furnitures from "../../images.json";
 
 // recolor te model
@@ -12,11 +13,8 @@ import furnitures from "../../images.json";
 // Add loading icon for suspense
 type FallingCubeProps = {
 	initialPosition: [number, number, number];
-	spawnHeight: number;
-	groundPos: number;
 	canvasRef: RefObject<HTMLDivElement | null>;
 	glbPath: string;
-	furnitureRef: RefObject<RapierRigidBody | null>;
 };
 
 function getRandomIntInclusive(min: number, max: number) {
@@ -28,21 +26,51 @@ function getRandomIntInclusive(min: number, max: number) {
 const RANDOMMIN = -50;
 const RANDOMMAX = 50;
 
-function Furniture({
-	initialPosition,
-	spawnHeight,
-	groundPos,
-	canvasRef,
-	glbPath,
-	furnitureRef
-}: FallingCubeProps) {
-
+function Furniture({ initialPosition, canvasRef, glbPath }: FallingCubeProps) {
+	const furnitureRef = useRef<RapierRigidBody | null>(null);
+    const dragging = useRef(false)
+    const dragTarget = useRef(new THREE.Vector3());
+    const dragPlane = useRef(new THREE.Plane(new THREE.Vector3(0,1,0), 0))
 	const gltf = useGLTF(glbPath);
+	const spawnHeight = 250;
+	const groundPos = -1000;
+    const { camera, mouse, raycaster } = useThree();
+
+    const handlePointerDown = (e) => {
+  e.stopPropagation()
+  dragging.current = true
+  furnitureRef.current.setBodyType(RigidBodyType.KinematicVelocityBased, true)
+  furnitureRef.current.setGravityScale(0, true)
+  const current =furnitureRef.current.translation()
+  const currentVec3 = new THREE.Vector3(current.x, current.y, current.z)
+
+  dragPlane.current.setFromNormalAndCoplanarPoint(
+    camera.getWorldDirection(new THREE.Vector3()).negate(),
+    currentVec3
+  )
+}
+
+const handlePointerUp = (e) => {
+  dragging.current = false
+  furnitureRef.current.setGravityScale(1, true)
+  furnitureRef.current.setBodyType(RigidBodyType.Dynamic, true)
+}
+
+      useFrame(() => {
+    if (!dragging.current) return
+
+    raycaster.setFromCamera(mouse, camera)
+    raycaster.ray.intersectPlane(
+      dragPlane.current,
+      dragTarget.current
+    )
+  })
 
 	useFrame(({ camera }) => {
-		if (!furnitureRef.current) return;
+		if (!furnitureRef.current || dragging.current) return;
+        console.log("camera and not dragging(?)", dragging.current)
 
-		const { y } = furnitureRef.current.translation()
+		const { y } = furnitureRef.current.translation();
 
 		if (y < groundPos) {
 			let width = 0;
@@ -87,27 +115,65 @@ function Furniture({
 		}
 	});
 
+    useFrame(() => {
+        if (!dragging.current || !furnitureRef.current) return 
+
+        const current = furnitureRef.current.translation();
+        const target = dragTarget.current;
+        raycaster.setFromCamera(mouse, camera)
+  raycaster.ray.intersectPlane(dragPlane.current, dragTarget.current)
+
+        furnitureRef.current.setLinvel(
+    {
+      x: (target.x - current.x) * 10,
+      y: (target.y - current.y) * 10,
+      z: 0,
+    },
+    true
+  )
+    })
+
 	return (
-		<RigidBody
-			ref={furnitureRef}
-			colliders="cuboid"
-			restitution={2}
-			angularDamping={0.2}
-			angularVelocity={[0.3, 0.3, 0]}
-			position={initialPosition}
+		<DragControls
+			objects={furnitureRef.current ? [furnitureRef.current] : []}
+			tranformGroup={false}
+			// onDragStart={() => {
+			// 	if (furnitureRef.current) {
+            //         dragging.current = true;
+			// 		furnitureRef.current.setBodyType(
+			// 			RigidBodyType.KinematicVelocityBased,
+			// 			true,
+			// 		);
+            //         furnitureRef.current.setGravityScale(0, true)
+            //         furnitureRef.current.setLinvel({ x: 0, y: 0, z: 0}, true)
+            //         dragPlane.current.set(new THREE.Vector3(0, 1, 0), -furnitureRef.current.translation().y)
+			// 	}
+			// }}
+			// onDragEnd={() => {
+			// 	if (furnitureRef.current) {
+            //         dragging.current = false; 
+            //         furnitureRef.current.setGravityScale(1, true);
+			// 		furnitureRef.current.setBodyType(RigidBodyType.Dynamic, true);
+			// 	}
+			// }}
 		>
-			<primitive object={gltf.scene.clone()} />
-		</RigidBody>
+			<RigidBody
+				ref={furnitureRef}
+				colliders="cuboid"
+				restitution={2}
+				angularDamping={0.2}
+				angularVelocity={[0.3, 0.3, 0]}
+				position={initialPosition}
+			>
+				<primitive object={gltf.scene} onPointerUp={handlePointerUp} onPointerDown={handlePointerDown}/>
+			</RigidBody>
+		</DragControls>
 	);
 }
 
 export const TestScene = () => {
 	const projects = furnitures.map(({ glb: [glb] }) => glb);
 	const canvasRef = useRef<HTMLDivElement>(null);
-	const furnitureRef = useRef<RapierRigidBody | null>(null);
-
-	const spawnHeight = 250;
-	const groundPos = -1000;
 	const gravity = getRandomIntInclusive(RANDOMMIN, RANDOMMAX);
 
 	return (
@@ -119,35 +185,16 @@ export const TestScene = () => {
 					<Physics gravity={[gravity, -20, 0]} debug>
 						{projects.map((project) => {
 							return (
-								<DragControls
-									key={project+"dragcontrol"}
-									objects={furnitureRef.current ? [furnitureRef.current] : []}
-									tranformGroup={false}
-									onDragStart={() => {
-											if (furnitureRef.current) {
-												furnitureRef.current.setBodyType(RigidBodyType.KinematicPositionBased, true);
-											}
-										
-									}}
-									onDragEnd={() => {
-											if (furnitureRef.current) {
-                                                furnitureRef.current.setBodyType(RigidBodyType.Dynamic, true);
-                                            }
-									}}
-								>
-									<Furniture
-										initialPosition={[
-											getRandomIntInclusive(RANDOMMIN, RANDOMMAX),
-											250,
-											0,
-										]}
-										spawnHeight={spawnHeight}
-										groundPos={groundPos}
-										canvasRef={canvasRef}
-										glbPath={project}
-										furnitureRef={furnitureRef}
-									/>
-								</DragControls>
+								<Furniture
+									key={project}
+									initialPosition={[
+										getRandomIntInclusive(RANDOMMIN, RANDOMMAX),
+										250,
+										0,
+									]}
+									canvasRef={canvasRef}
+									glbPath={project}
+								/>
 							);
 						})}
 					</Physics>
